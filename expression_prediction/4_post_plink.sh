929345ab46018f15298c600fca7cf2d13ffdf62d
karbalaei <reza80@gmail.com>
2025-10-30 17:56:08 -0400

    Update 4_post_plink.sh

--- expression_prediction/4_post_plink.sh
+++ expression_prediction/4_post_plink.sh
@@ -13,6 +13,53 @@ cd ../twas/Amygdala_gene
 
 find ./expression_prediction -type f -name "*.profile" -exec basename {} \; > ./Amygdala_gene_list_prediced.txt.profile
 
+
+library(data.table)
+
+# --- 1. Define Paths and Files ---
+file_list_path <- "Amygdala_gene_list_prediced.txt.profile"
+output_file <- "merged_prediced_expression_Amygdala.tsv"
+
+INPUT_DIR <- "./expression_prediction/"
+
+# Read the list of files to merge
+files_to_merge <- readLines(file_list_path)
+
+# --- 2. Function to Extract Gene Name ---
+# Extracts "gene_9710" from "gene_9710_prediction.profile"
+extract_gene_name <- function(filename) {
+  # Remove everything from "_prediction.profile" to the end
+  sub("_prediction\\.profile$", "", filename)
+}
+
+# --- 3. Iterate and Merge ---
+
+# Read the first file to initialize the master data table
+cat("Reading and initializing with:", files_to_merge[1], "\n")
+master_dt <- fread(files_to_merge[1], select = c(1, 2, 6)) # Select Col 1, 2, and 6
+setnames(master_dt, c("V1", "V2", "V6"), c("ID1", "ID2", extract_gene_name(files_to_merge[1])))
+
+# Iterate through the rest of the files (starting from the second file)
+for (i in 2:length(files_to_merge)) {
+  current_file_base <- files_to_merge[i]
+  current_file_path <- paste0(INPUT_DIR, current_file_base) # CONSTRUCT FULL PATH
+  current_gene <- extract_gene_name(current_file_base)
+  cat("Merging:", current_file_path, "\n")
+  
+  # Read the current file, selecting only the necessary columns
+  current_dt <- fread(current_file_path, select = c(1, 2, 6))
+  # Rename columns immediately for merging
+  setnames(current_dt, c("ID1", "ID2", current_gene))
+  
+  # Perform the merge
+  master_dt <- merge(master_dt, current_dt, 
+                     by = c("ID1", "ID2"), all = TRUE)
+}
+# --- 4. Write Final Output ---
+fwrite(master_dt, file = output_file, sep = "\t", na = "NA")
+
+cat("Merge complete. Output saved to:", output_file, "\n")
+
 # Define the output directory
 OUTPUT_DIR="./expression_prediction"
 mkdir -p ${OUTPUT_DIR}
